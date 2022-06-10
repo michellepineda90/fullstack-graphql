29733c814c99d27d7e78ae152610ecf6936c3d34
Michele Pineda <michelle.pineda.rivas@gmail.com>
2022-06-10 13:15:24 -0600

    chore: add code along to exercise files

--- client/src/client.js
+++ client/src/client.js
@@ -1,8 +1,32 @@
 import { ApolloClient } from 'apollo-client'
 import { InMemoryCache } from 'apollo-cache-inmemory'
 import { HttpLink } from 'apollo-link-http'
+import { ApolloLink } from 'apollo-link'
+import { setContext } from 'apollo-link-context'
 import gql from 'graphql-tag'
 
-/**
- * Create a new apollo client and export as default
- */
+const http = new HttpLink({uri: 'http://localhost:4000/'})
+
+// TODO: ADD DELAY
+const delay = setContext(request => {
+    new Promise ((success) => {
+        setTimeout(() => {
+            success()
+        }, 800)
+    })
+})
+
+// TODO: ADD LINK AND PUT THEM IN THE ORDER IN WHICH WE WANT THEM TO RUN
+const link = ApolloLink.from([
+    delay,
+    http
+])
+
+const cache = new InMemoryCache()
+
+const client = new ApolloClient({
+    link,
+    cache
+})
+
+export default client
--- client/src/pages/Pets.js
+++ client/src/pages/Pets.js
@@ -6,12 +6,67 @@ import NewPetModal from '../components/NewPetModal'
 import Loader from '../components/Loader'
 
 
+// operation type + operation name
+const ALL_PETS = gql`
+  query AllPets {
+    pets {
+      id
+      name
+      type
+      img
+    }
+  }
+`
+
+// operation type + operation name
+// name of the mutation
+const ADD_PET = gql `
+  mutation CreatePet($newPet: NewPetInput!) {
+    addPet(input: $newPet) { 
+      id
+      name
+      type
+      img
+    }
+}
+`
+
 export default function Pets () {
   const [modal, setModal] = useState(false)
-
+  const {data, loading, error} = useQuery(ALL_PETS)
+  const [createPet, newPet] = useMutation(ADD_PET, {
+    update(cache, {data: {addPet}}) {
+      const data = cache.readQuery({query:ALL_PETS})
+      cache.writeQuery({
+        query: ALL_PETS,
+        data: {pets: [addPet, ...data.pets]}
+      })
+    }
+  })
 
   const onSubmit = input => {
     setModal(false)
+    createPet({
+      variables: { newPet: input },
+      optimisticResponse: {
+        __typename: 'Mutation',
+        addPet: {
+          __typename: 'Pet',
+          id: Math.random(Math.random() * 10000) + '',
+          name: input.name,
+          type: input.type,
+          img: 'https://via.placeholder.com/150',
+        }
+      },
+    })
+  }
+
+  if (loading) {
+    return <Loader />
+  }
+
+  if (error || newPet.error) {
+    return <p>Error!</p>
   }
   
   if (modal) {
@@ -32,7 +87,7 @@ export default function Pets () {
         </div>
       </section>
       <section>
-        <PetsList />
+        <PetsList pets={data.pets}/>
       </section>
     </div>
   )
